2175c362e8a5e3aafa9623fbeea8e91c7d0e8163
Jerry1098 <jeremias1098@mailbox.org>
2025-04-15 18:15:14 +0000

    add no_auth_mqtt example

---
+++ mqtt_led/README.md
@@ -0,0 +1,4 @@
+
+
+# Short example
+This example was limited by the missing auth functionality of [rust-mqtt](https://github.com/obabec/rust-mqtt) which was a requirement for me.
--- mqtt_led_rust_mqtt_no_auth/src/bin/main.rs
+++ mqtt_led_rust_mqtt_no_auth/src/bin/main.rs
@@ -25,8 +25,8 @@ use smoltcp::wire::DnsQueryType;
 
 extern crate alloc;
 
-const SSID: &str = "Place";
-const PASSWORD: &str = "holder";
+const SSID: &str = "Neuland";
+const PASSWORD: &str = "GMKspY7A8brqw63y";
 
 const MQTT_FQDN: &str = "homeassistant";
 const MQTT_PORT: u16 = 1883;
